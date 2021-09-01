b6f831de9ac16fd9fec8af0dc6063fe11a5e7549
Michal Gawor <michal.j.gawor@gmail.com>
2021-09-01 12:37:06 +0200

    Exception raise if query param '?pid=' missing

--- dog_apps/dog_api/views_api.py
+++ dog_apps/dog_api/views_api.py
@@ -1,23 +1,24 @@
 from rest_framework import status
 from rest_framework.decorators import api_view
+from rest_framework.exceptions import MethodNotAllowed, ParseError
 from rest_framework.response import Response
 
 from .models import dog
 
 
-@api_view()
+@api_view(['GET'])
 def get_fetch(request):
-    if request.method == 'GET':
-        pid_candidate = request.query_params.get('pid', '')
-        fetch_result = dog.fetch(pid_candidate)
-        return Response(fetch_result)
-    else:
-        return Response({"Error:" "Requests other than GET not supported"}, status=status)
+    pid_candidate = request.query_params.get('pid')
+    if pid_candidate is None:
+        raise ParseError(detail="Missing query parameter 'pid'", code=400)
+    fetch_result = dog.fetch(pid_candidate)
+    return Response(fetch_result)
 
 
 @api_view(['GET'])
 def get_sniff(request):
-
-    pid_candidate = request.query_params.get('pid', '')
+    pid_candidate = request.query_params.get('pid')
+    if pid_candidate is None:
+        raise ParseError(detail="Missing query parameter 'pid'", code=400)
     sniff_result = dog.sniff(pid_candidate)
     return Response(sniff_result)
