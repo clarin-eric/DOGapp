14ad27c1a7482c11ca9de2342a1dd32b72d73ed8
gawor <michal.j.gawor@gmail.com>
2022-02-02 16:55:31 +0100

    track schemas

---
+++ dog_apps/dog_api/schemas.py
@@ -0,0 +1,40 @@
+from drf_yasg import openapi
+
+pid_queryparam: openapi.Parameter = openapi.Parameter(
+    name='pid',
+    in_=openapi.IN_QUERY,
+    description='Persistent identifier to a collection',
+    type=openapi.TYPE_ARRAY,
+    items=openapi.Items(type=openapi.TYPE_STRING))
+
+fetch_response_schema: openapi.Schema = openapi.Schema(
+    title='Fetch response schema',
+    description='Schema of the response of doglib.fetch() call',
+    type=openapi.TYPE_OBJECT,
+    additional_properties=openapi.Schema(
+        type=openapi.TYPE_OBJECT,
+        additional_properties=True
+    )
+)
+
+identify_response_schema: openapi.Schema = openapi.Schema(
+    title='Identify response schema',
+    description='Schema of the response of doglib.identify() call',
+    type=openapi.TYPE_OBJECT,
+    additional_properties=openapi.Schema(
+        type=openapi.TYPE_OBJECT,
+        additional_properties=openapi.Schema(
+            type=openapi.TYPE_STRING
+        )
+    )
+)
+
+sniff_response_schema: openapi.Schema = openapi.Schema(
+    title='Identify response schema',
+    description='Schema of the response of doglib.identify() call',
+    type=openapi.TYPE_OBJECT,
+    additional_properties=openapi.Schema(
+        type=openapi.TYPE_OBJECT,
+        additional_properties=True
+    )
+)
