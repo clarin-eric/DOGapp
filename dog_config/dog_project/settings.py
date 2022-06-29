bdb54652bc2507fab8db30a912eda5259ae1174e
gawor <michal.j.gawor@gmail.com>
2022-06-29 18:04:08 +0200

    rdf swagger in installed apps

--- dog_config/dog_project/settings.py
+++ dog_config/dog_project/settings.py
@@ -19,6 +19,8 @@ BASE_DIR = dirname(dirname(__file__))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY ='testsecret087B*#bAIUSd'
 ## Secure cookies have to be turned off in development mode, assuming there is
 ## no reverse proxy with X-Forwarded-Proto=https or https://tools.ietf.org/html/rfc7239.
 DEBUG = True # TODO: templatize
@@ -57,6 +59,7 @@ INSTALLED_APPS = [
     'doglib',
     'drf_yasg',
     'rest_framework',
+    'rest_framework_swagger',
 ]
 
 REST_FRAMEWORK = {
