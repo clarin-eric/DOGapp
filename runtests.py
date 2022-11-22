d05a2b202697bdd7994734e7b54528a70367630f
gawor <michal.j.gawor@gmail.com>
2022-11-22 16:32:26 +0100

    track runtests.py

---
+++ runtests.py
@@ -0,0 +1,28 @@
+import sys
+
+import django
+from django.conf import settings
+from django.test.utils import get_runner
+
+if __name__ == "__main__":
+    settings.configure(
+        DEBUG=True,
+        USE_TZ=True,
+        REST_FRAMEWORK={
+            'DEFAULT_RENDERER_CLASSES': [
+                'rest_framework.renderers.JSONRenderer',
+                'rest_framework.renderers.BrowsableAPIRenderer',
+            ],
+        },
+        INSTALLED_APPS=[
+            "django.contrib.auth",
+            "django.contrib.contenttypes",
+            "django.contrib.sites",
+            "dogapi",
+        ],
+    )
+    django.setup()
+    TestRunner = get_runner(settings)
+    test_runner = TestRunner()
+    failures = test_runner.run_tests(["dogapi.tests"])
+    sys.exit(bool(failures))
