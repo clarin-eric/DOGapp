c46da67987d85233ab12561749ea792cb339472a
gawor <michal.j.gawor@gmail.com>
2022-11-24 17:30:55 +0100

    missing possitional argument for parse_queryparam in tests

--- dogapi/dogapi/tests.py
+++ dogapi/dogapi/tests.py
@@ -16,21 +16,21 @@ class TestQueryParamParsing(unittest.TestCase):
         request_single_pid: Request = self.requestfactory.get('/fetch/?pid[]=val1')
         request_triple_pid: Request = self.requestfactory.get('/fetch/?pid[]=val1&pid[]=val2&pid[]=val3')
 
-        self.assertTrue(parse_pid_queryparam(request_single_pid) == self.single_pid and
-                        parse_pid_queryparam(request_triple_pid) == self.triple_pid)
+        self.assertTrue(parse_queryparam(request_single_pid) == self.single_pid and
+                        parse_queryparam(request_triple_pid) == self.triple_pid)
 
     def test_php_params_without_brackets(self):
         request_single_pid: Request = self.requestfactory.get('/fetch/?pid=val1')
         request_triple_pid: Request = self.requestfactory.get('/fetch/?pid=val1&pid=val2&pid=val3')
 
-        self.assertTrue(parse_pid_queryparam(request_single_pid) == self.single_pid and
-                        parse_pid_queryparam(request_triple_pid) == self.triple_pid)
+        self.assertTrue(parse_queryparam(request_single_pid) == self.single_pid and
+                        parse_queryparam(request_triple_pid) == self.triple_pid)
 
     def test_swagger_params_format(self):
         request_single_pid: Request = self.requestfactory.get('/fetch/?pid=val1')
         request_triple_pid: Request = self.requestfactory.get('/fetch/?pid=val1,val2,val3')
 
-        self.assertTrue(parse_pid_queryparam(request_single_pid) == self.single_pid and
-                        parse_pid_queryparam(request_triple_pid) == self.triple_pid)
+        self.assertTrue(parse_queryparam(request_single_pid) == self.single_pid and
+                        parse_queryparam(request_triple_pid) == self.triple_pid)
 
 
