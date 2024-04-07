7727c26cef83b97520295fab0d417123eb58cc51
gawor <michal.j.gawor@gmail.com>
2024-04-07 21:08:46 +0200

    urls

--- dogconfig/dogproject/settings.py
+++ dogconfig/dogproject/settings.py
@@ -51,7 +51,7 @@ INTERNAL_IPS = ['127.0.0.1']
 
 # https://knasmueller.net/fix-djangos-debug-toolbar-not-showing-inside-docker
 import socket
-hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
+hostname, _, ips = socket.gethostbyname_ex("localhost")
 INTERNAL_IPS += [".".join(ip.split(".")[:-1] + ["1"]) for ip in ips]
 
 # Application definition
--- dogconfig/dogproject/urls.py
+++ dogconfig/dogproject/urls.py
@@ -5,16 +5,12 @@ from django.urls import include, path, re_path
 from drf_spectacular.views import SpectacularAPIView, SpectacularJSONAPIView
 from rest_framework.permissions import AllowAny
 
-<<<<<<< HEAD
-from dogapi.views_api import fetch, identify, is_pid, sniff
+
 from dogui.views_ui import home
-=======
-from dogapi.views_api import fetch, identify, sniff, expand_datatype
->>>>>>> main
+from dogapi.views_api import fetch, identify, sniff, is_pid, expand_datatype
 
 
 urlpatterns = [
-<<<<<<< HEAD
     re_path(r'api/openapi.yml', SpectacularAPIView.as_view(), name='openapi_schema_yml'),
     re_path(r'api/openapi.json', SpectacularJSONAPIView.as_view(), name='openapi_schema_json'),
 
@@ -24,13 +20,5 @@ urlpatterns = [
     path('api/ispid/', is_pid, name='is pid'),
     path('', home, name='main'),
     path('__debug__/', include('debug_toolbar.urls')),
-=======
-    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-    path('fetch/', fetch, name='fetch'),
-    path('identify/', identify, name='identify'),
-    path('sniff/', sniff, name='sniff'),
     path('expanddatatype/', expand_datatype, name='expand data type'),
->>>>>>> main
 ]
