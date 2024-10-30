e968f5387ff32ecf32b1a33c19365a8c64cbae33
gawor <michal.j.gawor@gmail.com>
2024-10-30 11:49:34 +0100

    API versioning in tests

--- dogapi/dogapi/tests.py
+++ dogapi/dogapi/tests.py
@@ -16,22 +16,22 @@ class TestQueryParamParsing(unittest.TestCase):
         cls.triple_pid = ['val1', 'val2', 'val3']
 
     def test_php_params_with_brackets(self):
-        request_single_pid: Request = self.request_factory.get('/fetch/?pid[]=val1')
-        request_triple_pid: Request = self.request_factory.get('/fetch/?pid[]=val1&pid[]=val2&pid[]=val3')
+        request_single_pid: Request = self.request_factory.get('/api/v1/fetch/?pid[]=val1')
+        request_triple_pid: Request = self.request_factory.get('/api/v1/fetch/?pid[]=val1&pid[]=val2&pid[]=val3')
 
         self.assertTrue(parse_queryparam(request_single_pid, "pid") == self.single_pid and
                         parse_queryparam(request_triple_pid, "pid") == self.triple_pid)
 
     def test_php_params_without_brackets(self):
-        request_single_pid: Request = self.request_factory.get('/fetch/?pid=val1')
-        request_triple_pid: Request = self.request_factory.get('/fetch/?pid=val1&pid=val2&pid=val3')
+        request_single_pid: Request = self.request_factory.get('/api/v1/fetch/?pid=val1')
+        request_triple_pid: Request = self.request_factory.get('/api/v1/fetch/?pid=val1&pid=val2&pid=val3')
 
         self.assertTrue(parse_queryparam(request_single_pid, "pid") == self.single_pid and
                         parse_queryparam(request_triple_pid, "pid") == self.triple_pid)
 
     def test_swagger_params_format(self):
-        request_single_pid: Request = self.request_factory.get('/fetch/?pid=val1')
-        request_triple_pid: Request = self.request_factory.get('/fetch/?pid=val1,val2,val3')
+        request_single_pid: Request = self.request_factory.get('/api/v1/fetch/?pid=val1')
+        request_triple_pid: Request = self.request_factory.get('/api/v1/fetch/?pid=val1,val2,val3')
 
         self.assertTrue(parse_queryparam(request_single_pid, "pid") == self.single_pid and
                         parse_queryparam(request_triple_pid, "pid") == self.triple_pid)
@@ -45,13 +45,13 @@ class TestFetchEndpoint(unittest.TestCase):
 
     def test_single_correct_pid(self):
         pid = "https://lindat.mff.cuni.cz/repository/xmlui/handle/11234/1-3422"
-        response = self.client.get(f"/api/fetch/?pid={pid}")
+        response = self.client.get(f"/api/v1/fetch/?pid={pid}")
         # if correct failure code will be 0, note that 200 response code does not imply success
         self.assertFalse(response.json()["https://lindat.mff.cuni.cz/repository/xmlui/handle/11234/1-3422"]["failure"])
 
     def test_multiple_correct_pid(self):
         pids = ["https://lindat.mff.cuni.cz/repository/xmlui/handle/11234/1-3422", "https://b2share.eudat.eu/records/d64361c0a6384760a8a8f32e0dc4a481"]
-        response = self.client.get(f"/api/fetch/?pid={','.join(pids)}")
+        response = self.client.get(f"/api/v1/fetch/?pid={','.join(pids)}")
         # if correct failure code will be 0, note that 200 response code does not imply success
         self.assertFalse(
             response.json()["https://lindat.mff.cuni.cz/repository/xmlui/handle/11234/1-3422"]["failure"] and
@@ -59,20 +59,20 @@ class TestFetchEndpoint(unittest.TestCase):
 
     def test_single_incorrect_pid(self):
         pid = "abcd"
-        response = self.client.get(f"/api/fetch/?pid={pid}")
+        response = self.client.get(f"/api/v1/fetch/?pid={pid}")
         # if correct failure code will be 0, note that 200 response code does not imply success
         self.assertTrue(response.json()["https://lindat.mff.cuni.cz/repository/xmlui/handle/11234/1-3422"]["failure"])
 
     def test_multiple_incorrect_pid(self):
         pids = ["abcd", "efgh"]
-        response = self.client.get(f"/api/fetch/?pid={','.join(pids)}")
+        response = self.client.get(f"/api/v1/fetch/?pid={','.join(pids)}")
         # if correct failure code will be 0, note that 200 response code does not imply success
         self.assertTrue(response.json()["abcd"]["failure"] and
                         response.json()["efgh"]["failure"])
 
     def test_mixed_correct_incorrect_pid(self):
         pids = ["https://lindat.mff.cuni.cz/repository/xmlui/handle/11234/1-3422", "abcd"]
-        response = self.client.get(f"/api/fetch/?pid={','.join(pids)}")
+        response = self.client.get(f"/api/v1/fetch/?pid={','.join(pids)}")
         # if correct failure code will be 0, note that 200 response code does not imply success
         self.assertTrue(
             not response.json()["https://lindat.mff.cuni.cz/repository/xmlui/handle/11234/1-3422"]["failure"]
@@ -87,13 +87,13 @@ class TestSniffEndpoint(unittest.TestCase):
 
     def test_single_correct_pid(self):
         pid = "https://lindat.mff.cuni.cz/repository/xmlui/handle/11234/1-3422"
-        response = self.client.get(f"/api/sniff/?pid={pid}")
+        response = self.client.get(f"/api/v1/sniff/?pid={pid}")
         # if correct failure code will be 0, note that 200 response code does not imply success
         self.assertFalse(response.json()["https://lindat.mff.cuni.cz/repository/xmlui/handle/11234/1-3422"]["failure"])
 
     def test_multiple_correct_pid(self):
         pids = ["https://lindat.mff.cuni.cz/repository/xmlui/handle/11234/1-3422", "https://b2share.eudat.eu/records/d64361c0a6384760a8a8f32e0dc4a481"]
-        response = self.client.get(f"/api/sniff/?pid={','.join(pids)}")
+        response = self.client.get(f"/api/v1/sniff/?pid={','.join(pids)}")
         # if correct failure code will be 0, note that 200 response code does not imply success
         self.assertFalse(
             response.json()["https://lindat.mff.cuni.cz/repository/xmlui/handle/11234/1-3422"]["failure"] and
@@ -101,20 +101,20 @@ class TestSniffEndpoint(unittest.TestCase):
 
     def test_single_incorrect_pid(self):
         pid = "abcd"
-        response = self.client.get(f"/api/sniff/?pid={pid}")
+        response = self.client.get(f"/api/v1/sniff/?pid={pid}")
         # if correct failure code will be 0, note that 200 response code does not imply success
         self.assertTrue(response.json()["https://lindat.mff.cuni.cz/repository/xmlui/handle/11234/1-3422"]["failure"])
 
     def test_multiple_incorrect_pid(self):
         pids = ["abcd", "efgh"]
-        response = self.client.get(f"/api/sniff/?pid={','.join(pids)}")
+        response = self.client.get(f"/api/v1/sniff/?pid={','.join(pids)}")
         # if correct failure code will be 0, note that 200 response code does not imply success
         self.assertTrue(response.json()["abcd"]["failure"] and
                         response.json()["efgh"]["failure"])
 
     def test_mixed_correct_incorrect_pid(self):
         pids = ["https://lindat.mff.cuni.cz/repository/xmlui/handle/11234/1-3422", "abcd"]
-        response = self.client.get(f"/api/sniff/?pid={','.join(pids)}")
+        response = self.client.get(f"/api/v1/sniff/?pid={','.join(pids)}")
         # if correct failure code will be 0, note that 200 response code does not imply success
         self.assertTrue(
             not response.json()["https://lindat.mff.cuni.cz/repository/xmlui/handle/11234/1-3422"]["failure"]
@@ -129,31 +129,31 @@ class TestIsPIDEndpoint(unittest.TestCase):
 
     def test_single_correct_pid(self):
         pid = "https://lindat.mff.cuni.cz/repository/xmlui/handle/11234/1-3422"
-        response = self.client.get(f"/api/ispid/?pid={pid}")
+        response = self.client.get(f"/api/v1/ispid/?pid={pid}")
         self.assertTrue(response.json()["https://lindat.mff.cuni.cz/repository/xmlui/handle/11234/1-3422"])
 
     def test_multiple_correct_pid(self):
         pids = ["https://lindat.mff.cuni.cz/repository/xmlui/handle/11234/1-3422",
                 "https://b2share.eudat.eu/records/d64361c0a6384760a8a8f32e0dc4a481"]
-        response = self.client.get(f"/api/ispid/?pid={','.join(pids)}")
+        response = self.client.get(f"/api/v1/ispid/?pid={','.join(pids)}")
         self.assertTrue(
             response.json()["https://lindat.mff.cuni.cz/repository/xmlui/handle/11234/1-3422"] and
             response.json()["https://b2share.eudat.eu/records/d64361c0a6384760a8a8f32e0dc4a481"])
 
     def test_single_incorrect_pid(self):
         pid = "abcd"
-        response = self.client.get(f"/api/sniff/?pid={pid}")
+        response = self.client.get(f"/api/v1/sniff/?pid={pid}")
         self.assertFalse(response.json()["abcd"])
 
     def test_multiple_incorrect_pid(self):
         pids = ["abcd", "efgh"]
-        response = self.client.get(f"/api/sniff/?pid={','.join(pids)}")
+        response = self.client.get(f"/api/v1/sniff/?pid={','.join(pids)}")
         self.assertTrue(not response.json()["abcd"] and
                         not response.json()["efgh"])
 
     def test_mixed_correct_incorrect_pid(self):
         pids = ["https://lindat.mff.cuni.cz/repository/xmlui/handle/11234/1-3422", "abcd"]
-        response = self.client.get(f"/api/sniff/?pid={','.join(pids)}")
+        response = self.client.get(f"/api/v1/sniff/?pid={','.join(pids)}")
         self.assertTrue(
             not response.json()["abcd"]
             and response.json()["https://lindat.mff.cuni.cz/repository/xmlui/handle/11234/1-3422"]
