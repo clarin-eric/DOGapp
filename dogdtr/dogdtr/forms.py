eff2c2b4d6cfabd9342984271839af0f070c4876
gawor <michal.j.gawor@gmail.com>
2024-11-05 08:47:33 +0100

    dtr

---
+++ dogdtr/dogdtr/forms.py
@@ -0,0 +1,42 @@
+from django import forms
+from django.core.exceptions import ValidationError
+from django.utils.safestring import mark_safe
+from typing import List
+
+from dogapi.models import dog
+
+
+class MIMETypeField(forms.CharField):
+    def to_python(self, pid_candidates):
+        """String to list of strings"""
+        if not pid_candidates:
+            return []
+        return pid_candidates.split(',')
+
+    def validate(self, pid_candidates):
+        """Check if all pids"""
+        super().validate(pid_candidates)
+        # TODO ask for local validation vs api call
+        # for pid_candidate in pid_candidates:
+        #     if dog.is_pid(pid_candidate):
+        #         raise ValidationError(
+        #             '%(pid_candidate) is not a PID accepted by DOG. Make sure it is a valid DOI, HDL or URL',
+        #             params={'pid_candidate': pid_candidate},
+        #         )
+
+    def clean(self, value):
+        value = self.to_python(value)
+        self.validate(value)
+        return value
+
+
+class MIMETypeForm(forms.Form):
+    """
+    Input form for inserting MIMEtype
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["mimetype"].label = ""
+
+    mimetype_field: MIMETypeField = MIMETypeField(required=True,
+                                                  widget=forms.TextInput(attrs={'required': 'True'}))
--- dogdtr/dogdtr/views_ui.py
+++ dogdtr/dogdtr/views_ui.py
@@ -6,13 +6,27 @@ import logging.config
 
 
 from .utils import TaxonomyTree
+from .forms import MIMETypeForm
 
 logging.config.dictConfig(settings.LOGGING)
+
 API_NETLOC = settings.API_NETLOC
 DTR_ENABLED = settings.DTR_ENABLED
+VERIFY_SSL = settings.VERIFY_SSL
 
 
 def dtr(request: HttpRequest) -> HttpResponse:
+    context: RequestContext = RequestContext(request)
+    mimetype_form: MIMETypeForm = MIMETypeForm(request.GET)
+
+    if pid_form.is_valid():
+        context.push({"mimetype_form": mimetype_form})
+        pids = mimetype_form.cleaned_data['mimetype_field']
+        api_url = API_NETLOC + f'/expanddatatype/?data_type={",".join(pids)}'
+
+        api_response = requests.get(api_url, verify=VERIFY_SSL)
+
+
     context: RequestContext = RequestContext(request)
     context.push({"view": "dtr"})
     context.push({"DTR_ENABLED": DTR_ENABLED})
