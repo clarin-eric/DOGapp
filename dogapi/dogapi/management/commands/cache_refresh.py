02fad7b95178bfb1e6ca896450ef167d3238c9a5
gawor <michal.j.gawor@gmail.com>
2024-11-06 13:49:10 +0100

    cache_refresh always forces cache refresh

--- dogapi/dogapi/management/commands/cache_refresh.py
+++ dogapi/dogapi/management/commands/cache_refresh.py
@@ -1,15 +1,14 @@
+from django.core.cache import cache
 from django.core.management.base import BaseCommand, CommandError
 from django.test.client import RequestFactory
 
-from dogapi.views_api import get_repositories_status
+from dogapi.models import dog
 
 
 class Command(BaseCommand):
     help = 'Refresh repository status cache'
 
     def handle(self, *args, **options):
-        factory = RequestFactory()
-        path = 'api/repostatus/'
-        request = factory.get(path)
-        get_repositories_status(request)
+        repositories_status = dog.get_all_repositories_status()
+        cache.set('repositories_status', repositories_status, 86400)
         self.stdout.write('Repository status refreshed')
