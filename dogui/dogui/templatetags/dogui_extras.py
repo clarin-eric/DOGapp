d2a7ee905e8171dbd3c8fc0208d3ae67adc7727d
MichalGawor <michal.j.gawor@gmail.com>
2026-03-06 10:48:27 +0100

    custom filter for escaping urls for switchboard

--- dogui/dogui/templatetags/dogui_extras.py
+++ dogui/dogui/templatetags/dogui_extras.py
@@ -1,6 +1,7 @@
 from django import template
 import sys
 from typing import Any, Type
+import urllib.parse
 
 register = template.Library()
 
@@ -19,3 +20,13 @@ def is_instance(value: Any, class_name) -> bool:
     value = exec(value)
     class_type = exec(class_name)
     return isinstance(value, class_type)
+
+
+@register.filter
+def url_escape_path(value):
+    # safe=':/' tells Python: "Don't touch the protocol or the slashes"
+    # This turns spaces into %20 but keeps https:// intact
+    if value:
+        # Encode everything EXCEPT the structural characters of a URL
+        return urllib.parse.quote(value, safe=':/?&=#')
+    return value
