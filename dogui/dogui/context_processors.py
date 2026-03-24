1ace816eb42c3c2d6619337a980be1076a3e40dc
MichalGawor <michal.j.gawor@gmail.com>
2026-03-24 13:36:56 +0100

    Production instance added to context processor and templates

--- dogui/dogui/context_processors.py
+++ dogui/dogui/context_processors.py
@@ -7,9 +7,12 @@ def version(request):
     # pylint: disable=unused-argument
     version = settings.VERSION
     print(version)
-    alpha_regex = re.compile(r"^[0-9]+\.[0-9]+\.[0-9]+(-)?(a|b|rc)[0-9]+$")
-    beta_regex = re.compile(r"^[0-9]+\.[0-9]+\.[0-9]+$")
+    alpha_regex = re.compile(r"^[0-9]+\.[0-9]+\.[0-9]+(-)?a[0-9]+$")
+    beta_regex = re.compile(r"^[0-9]+\.[0-9]+\.[0-9]+(-)?(b|rc)[0-9]+$")
+    production_regex = re.compile(r"^[0-9]+\.[0-9]+\.[0-9]+$")
     if alpha_regex.match(version):
         return {"VERSION": version, "INSTANCE": "ALPHA"}
     elif beta_regex.match(version):
         return {'VERSION': version, "INSTANCE": "BETA"}
+    elif production_regex.match(version):
+        return {'VERSION': version, "INSTANCE": ""}
